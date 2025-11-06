f8cf37146dc34e080b30e6cb68840d799edce1fe
“xxkaro” <dunal.karolina@gmail.com>
2025-11-06 18:23:33 +0100

    model test fix

--- code/tests/model_test.py
+++ code/tests/model_test.py
@@ -1,22 +1,31 @@
-# write simple test to check if vllm model works properly on a random picture and text input
 import unittest
 from code.models.vllm import VLLMFactory
 from code.technical.content import ImageContent, TextContent
 import asyncio
+import os
 
 class TestVLLM(unittest.TestCase):
 
     def setUp(self):
-        self.vllm = VLLMFactory(model_name="test_model")
+        self.vllm = VLLMFactory(model_name="Qwen/Qwen2.5-VL-72B-Instruct")
 
     def test_vllm_with_text_input(self):
         text_content = TextContent("What is the capital of Norway?")
         response = asyncio.run(self.vllm.ask([text_content]))
         self.assertIn("Oslo", response)
 
-    def test_vllm_with_image_input(self):
+    def test_vllm_with_multimodal_input(self):
+        relative_path = "data_raw/bp/006/4.png"
+        full_path_to_check = os.path.abspath(relative_path)
+
+        self.assertTrue(
+            os.path.exists(full_path_to_check),
+            f"Test Aborted: Image file not found! "
+            f"Expected absolute path: {full_path_to_check}"
+        )
+
         text_content = TextContent("What shape do you see?")
-        image_content = ImageContent("../../../data_raw/bp/006/4.png")
+        image_content = ImageContent(relative_path)
         contents = [text_content, image_content]
         response = asyncio.run(self.vllm.ask(contents))
         self.assertIn("triangle", response)
