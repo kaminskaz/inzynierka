b824e24140c8d5efec2dc5b6641fb689336b5c55
“xxkaro” <dunal.karolina@gmail.com>
2025-11-29 17:35:37 +0100

    evaluation tests

--- code/tests/evaluation_test.py
+++ code/tests/evaluation_test.py
@@ -5,27 +5,37 @@ import os
 from pathlib import Path
 
 from code.evaluation.evaluation_basic import EvaluationBasic
+from code.evaluation.evaluation_judge import EvaluationWithJudge
 
 def main():
+    print("Evaluation standard")
     evaluator = EvaluationBasic()
-    # evaluator.evaluate(
-    #     results_dir="code/tests",
-    #     answers_path="code/tests/cvr_example_results.csv",
-    #     key_path="data_test/cvr/jsons/cvr_solutions.json",
-    #     evaluation_output_path="evaluation_results"
-    # )
-
     evaluator.run_evaluation(
         dataset_name="cvr",
-        model_name="example_model",
-        strategy_name="example_strategy",
+        model_name="Qwen2.5-VL-7B-Instruct",
+        strategy_name="contrastive",
         version="1",
-        results_dir="code/tests",
-        answers_path="code/tests/cvr_example_results.csv",
+        results_dir="code/results",
+        answers_path="code/results/contrastive_cvr_Qwen2.5-VL-7B-Instruct_ver1/results.csv",
         key_path="data_test/cvr/jsons/cvr_solutions.json",
         evaluation_output_path="evaluation_results",
         concat=True,
-        output_all_results_concat_path="code/tests/all_results_concat.csv"
+        output_all_results_concat_path="code/results/all_results_concat.csv"
+    )
+
+    print("Evaluation with LLM Judge")
+    evaluator = EvaluationWithJudge()
+    evaluator.run_evaluation(
+        dataset_name="bp",
+        model_name="Qwen2.5-VL-7B-Instruct",
+        strategy_name="direct",
+        version="1",
+        results_dir="code/results/direct_bp_Qwen2.5-VL-7B-Instruct_ver1",
+        answers_path="code/results/direct_bp_Qwen2.5-VL-7B-Instruct_ver1/results.csv",
+        key_path="data_test/bp/jsons/bp_solutions.json",
+        evaluation_output_path="evaluation_results",
+        concat=True,
+        output_all_results_concat_path="code/results/all_results_concat.csv"
     )
 
 if __name__ == "__main__":
