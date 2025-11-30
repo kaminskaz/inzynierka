5bc2b7413d1d869af6fbc860d76c9347f3920045
“xxkaro” <dunal.karolina@gmail.com>
2025-11-30 11:58:30 +0100

    evaluation test path fix

--- code/tests/evaluation_test.py
+++ code/tests/evaluation_test.py
@@ -15,12 +15,12 @@ def main():
         model_name="Qwen2.5-VL-7B-Instruct",
         strategy_name="contrastive",
         version="1",
-        results_dir="code/results",
-        answers_path="code/results/contrastive_cvr_Qwen2.5-VL-7B-Instruct_ver1/results.csv",
+        results_dir="results",
+        answers_path="results/contrastive_cvr_Qwen2.5-VL-7B-Instruct_ver1/results.csv",
         key_path="data_test/cvr/jsons/cvr_solutions.json",
         evaluation_output_path="evaluation_results",
         concat=True,
-        output_all_results_concat_path="code/results/all_results_concat.csv"
+        output_all_results_concat_path="results/all_results_concat.csv"
     )
 
     print("Evaluation with LLM Judge")
@@ -30,12 +30,12 @@ def main():
         model_name="Qwen2.5-VL-7B-Instruct",
         strategy_name="direct",
         version="1",
-        results_dir="code/results/direct_bp_Qwen2.5-VL-7B-Instruct_ver1",
-        answers_path="code/results/direct_bp_Qwen2.5-VL-7B-Instruct_ver1/results.csv",
+        results_dir="results/direct_bp_Qwen2.5-VL-7B-Instruct_ver1",
+        answers_path="results/direct_bp_Qwen2.5-VL-7B-Instruct_ver1/results.csv",
         key_path="data_test/bp/jsons/bp_solutions.json",
         evaluation_output_path="evaluation_results",
         concat=True,
-        output_all_results_concat_path="code/results/all_results_concat.csv"
+        output_all_results_concat_path="results/all_results_concat.csv"
     )
 
 if __name__ == "__main__":
