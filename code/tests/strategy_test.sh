bec94c3bb8fbc46bd39ff1a2eaa58f204e82b89f
kaminskaz <01180708@pw.edu.pl>
2025-12-08 22:13:07 +0100

    update

--- code/tests/strategy_test.sh
+++ code/tests/strategy_test.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=inz # Tu nazywasz jakoś swój proces, byle co szczerze mało warte bo i tak po nicku ja znajduje mój task
 #SBATCH --time=20:00:00 # dla short to masz max 2h dla long i experimental masz chyba 3-4 dni to jest czas po którym slurm ubja tw>
 #SBATCH --ntasks=1 # tutaj wystarczy 1 zawsze mieć chyba że chcesz multi gpu itp ale zapewne 1 GPU wam wystarczy
-#SBATCH --gpus=4 # Jak nie potrzebujesz GPU to wyrzucasz tą linijke
+#SBATCH --gpus=1 # Jak nie potrzebujesz GPU to wyrzucasz tą linijke
 #SBATCH --cpus-per-gpu=8 # Ile cpu na jedno gpu ma być w tym konfigu to po prostu ile cpu chcesz mieć mówiłem żeby dawać zawsze mi>
 #SBATCH --mem=128gb # Ile ram chcesz mieć mamy dużo więc nie musisz dawać mało ale bez przesady
 #SBATCH --partition=hopper # Tutaj podajesz short,long,experimental jedną z tych partycji z której chcesz korzystać shot i long ma>
@@ -46,7 +46,7 @@ python run_single_experiment.py \
     --max_tokens 16384 \
     --max_output_tokens 4096 \
     --limit_mm_per_prompt 2 \
-    --custom_args --tensor-parallel-size 4 --gpu-memory-utilization 0.9
+    --custom_args --tensor-parallel-size 1 --gpu-memory-utilization 0.9
     # --debug
 
 rm -rf ${JOB_HF_HOME}
